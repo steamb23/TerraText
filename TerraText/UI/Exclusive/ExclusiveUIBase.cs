e98e2d9696143e899aab33e129b9b8fb6611db89
steamb23 <steamb23@outlook.com>
2020-02-27 19:41:34 +0900

    비어있는 단독 UI 기본 클래스 추가

---
+++ TerraText/UI/Exclusive/ExclusiveUIBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerraText.UI.Exclusive
+{
+    /// <summary>
+    /// 단독 스크린 버퍼를 점유하는 UI의 최상위 클래스입니다.
+    /// </summary>
+    public abstract class ExclusiveUIBase
+    {
+        public ExclusiveUIBase()
+        {
+
+        }
+    }
+}
