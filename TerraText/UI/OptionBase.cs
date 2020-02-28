be2c0e89ba608c7f12e56eb9278e6b6543b9a6fa
steamb23 <steamb23@outlook.com>
2020-02-28 18:20:17 +0900

    UIBase, OptionBase 클래스 추가

---
+++ TerraText/UI/OptionBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerraText.UI
+{
+    /// <summary>
+    /// 옵션에 대한 기본 클래스를 나타냅니다.
+    /// </summary>
+    public abstract class OptionBase : UIBase
+    {
+
+        /// <summary>
+        /// 유저가 선택한 값을 가져옵니다. 선택된 값이 없으면 -1입니다.
+        /// </summary>
+        public int Result
+        {
+            get;
+            protected set;
+        } = -1;
+
+        /// <summary>
+        /// 옵션으로 출력될 텍스트의 목록입니다.
+        /// </summary>
+        public IReadOnlyCollection<string> Items { get; }
+        protected OptionBase(params string[] items)
+        {
+            Items = items;
+        }
+    }
+}
---
+++ TerraText/UI/UIBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerraText.UI
+{
+    /// <summary>
+    /// UI의 기본 클래스를 나타냅니다.
+    /// </summary>
+    public abstract class UIBase
+    {
+        /// <summary>
+        /// 기준점의 열 위치를 가져오거나 설정합니다.
+        /// </summary>
+        public int BaseLeft { get; set; }
+        /// <summary>
+        /// 기준점의 행 위치를 가져오거나 설정합니다.
+        /// </summary>
+        public int BaseTop { get; set; }
+        /// <summary>
+        /// 기준점의 위치를 설정합니다.
+        /// </summary>
+        /// <param name="left">기준점의 열 위치입니다.</param>
+        /// <param name="top">기준점의 행 위치입니다.</param>
+        public void SetBasePosition(int left, int top)
+        {
+            BaseLeft = left;
+            BaseTop = top;
+        }
+
+        /// <summary>
+        /// <see cref="UIBase"/> 클래스의 인스턴스를 초기화합니다.
+        /// </summary>
+        protected UIBase()
+        {
+            BaseLeft = Console.CursorLeft;
+            BaseTop = Console.CursorTop;
+        }
+        /// <summary>
+        /// UI요소를 출력하고 기능을 수행합니다.
+        /// </summary>
+        public abstract void Show();
+    }
+}
